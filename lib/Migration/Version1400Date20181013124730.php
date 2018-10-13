1bc95c755c32853b59a03eb738b5f8723e73a476
Julius Härtl <jus@bitgrid.net>
2018-10-13 14:53:43 +0200

    Move to migrations
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

---
+++ lib/Migration/Version1400Date20181013124730.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\NotesTutorial\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\SimpleMigrationStep;
+use OCP\Migration\IOutput;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ */
+class Version1400Date20181013124730 extends SimpleMigrationStep {
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options) {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+
+		if (!$schema->hasTable('notestutorial')) {
+			$table = $schema->createTable('notestutorial');
+			$table->addColumn('id', 'bigint', [
+				'autoincrement' => true,
+				'notnull' => true,
+				'length' => 8,
+				'unsigned' => true,
+			]);
+			$table->addColumn('title', 'string', [
+				'notnull' => true,
+				'length' => 200,
+				'default' => '',
+			]);
+			$table->addColumn('user_id', 'string', [
+				'notnull' => true,
+				'length' => 200,
+				'default' => '',
+			]);
+			$table->addColumn('content', 'text', [
+				'notnull' => true,
+				'default' => '',
+			]);
+			$table->setPrimaryKey(['id']);
+		}
+		return $schema;
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+	}
+}
