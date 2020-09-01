cffd26a1563c2c5b4a97a407aca9c4b5ab490d15
korelstar <korelstar@users.noreply.github.com>
2020-09-01 08:20:53 +0200

    simplify global vars
    Signed-off-by: npmbuildbot[bot] <npmbuildbot[bot]@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -22,10 +22,7 @@
 import Vue from 'vue'
 import App from './App'
 
-Vue.prototype.t = t
-Vue.prototype.n = n
-Vue.prototype.OC = OC
-Vue.prototype.OCA = OCA
+Vue.mixin({ methods: { t, n } })
 
 export default new Vue({
 	el: '#content',
