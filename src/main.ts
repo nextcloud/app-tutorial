5dd80e4a87671c6c2391b19a1894681b87c382d6
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2026-03-10 18:05:12 +0100

    feat: migrate to vue 3
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+/**
+ * SPDX-FileCopyrightText: 2018 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const app = createApp(App)
+app.mount('#content')
